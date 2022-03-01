0fb95a23a9852f32996cdde6e762c7e32efe807e
abir14016 <abir14016@gmail.com>
2022-03-02 00:40:30 +0600

    mealdb-api-js

---
+++ mealdb.js
@@ -0,0 +1,50 @@
+const searchFood = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    searchField.value = '';
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayFood(data.meals))
+}
+const displayFood = meals =>{
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    meals.forEach(meal =>{
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0,250)}</p>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    })
+}
+
+const loadMealDetail = mealId =>{
+    // console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetail(data.meals[0]))
+}
+displayMealDetail = meal =>{
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card')
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">${meal.strInstructions.slice(0,250)}</p>
+      <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    mealDetails.appendChild(div)
+}
